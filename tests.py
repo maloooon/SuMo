73155cd1fd24a28b0c4effe6eecbabe545777341
maloooon <118358326+maloooon@users.noreply.github.com>
2022-12-07 23:09:02 +0100

    GCN feature selection, mRNA full input structure

--- tests.py
+++ tests.py
@@ -3,8 +3,13 @@ import numpy as np
 import os
 import pandas as pd
 
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
 if __name__ == '__main__':
-    for x in range(5):
-        if x == 2:
-            print("ja")
-        print(x)
+    x = torch.rand(10,5)
+    x[3,4] = 10
+    print(x[3,4].item())
+
+    print(x)

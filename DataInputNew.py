cross validation, variance fix

DataInputNew.py
@@ -241,13 +241,45 @@ class SurvMultiOmicsDataModule(pl.LightningDataModule):
         self.x_train = [torch.nan_to_num(x_view) for x_view in self.x_train]
         self.x_train_mask = [torch.isnan(x_view) for x_view in self.x_train] #List of booleans for each numeric value in samples ;True : NaN values
 
+        # We check for each view how many 0s this view contains
+        train_zeros = []
+        for view in self.x_train:
+            curr_view_count = torch.count_nonzero(view)
+            train_zeros.append(curr_view_count)
+
+        removed_views_index = []
+        for x,count in enumerate(train_zeros):
+            # If there arent atleast 10 % values greater than 0 for this view for all samples, remove this view
+            # from consideration
+            if count < int(0.1 * ((self.feature_offsets[x + 1] - self.feature_offsets[x]) * n_train_samples)):
+                print("{} has nearly only 0 values. We don't take this data into consideration.".format(self.view_names[x]))
+                removed_views_index.append(x)
+
+
+
+
         self.x_test = [torch.nan_to_num(x_view) for x_view in self.x_test]
         self.x_test_mask = [torch.isnan(x_view) for x_view in self.x_test]
 
         self.x_val = [torch.nan_to_num(x_view) for x_view in self.x_val]
         self.x_val_mask = [torch.isnan(x_view) for x_view in self.x_val]
 
-        return n_train_samples, n_test_samples, n_val_samples
+        for index in sorted(removed_views_index, reverse=True):
+            del self.x_train[index]
+            del self.x_test[index]
+            del self.x_val[index]
+            del self.view_names[index]
+            diff = self.feature_offsets[index + 1] - self.feature_offsets[index]
+            for c,_ in enumerate(self.feature_offsets):
+                if c > (index + 1): # TODO :check if mRNA & RPPA gets deleted
+                    self.feature_offsets[c] = self.feature_offsets[c] - diff
+            del self.feature_offsets[index+1]
+            # TODO : also for mask if to be implemented ; ganze Prinzip msus auch für val und test set noch implementiert werden
+
+        self.n_views = self.n_views - len(removed_views_index) # TODO : wenn val/test set auch noch, muss removed views index train/val/test sei
+
+
+        return n_train_samples, n_test_samples, n_val_samples, self.view_names
 
 
     def label_transform(self, num_durations):
@@ -387,17 +419,36 @@ class SurvMultiOmicsDataModule(pl.LightningDataModule):
         if method.lower() == 'variance':
             variance_train_tensors = []
             variance_test_tensors = []
-            #variance_selected_features = []
-            thresholds = [0.8,0.8,0.8,0.6] # TODO : Grid Search
+            variance_val_tensors = []
+
+            thresholds = [0.8,0.8,0.8,0] # TODO : Grid Search
 
             for view in range(self.n_views):
+
                 view_train_variance = FeatureSelection.F_VARIANCE(self.x_train[view], threshold= thresholds[view])
+                view_val_variance = FeatureSelection.F_VARIANCE(self.x_val[view], threshold= thresholds[view])
                 view_test_variance = FeatureSelection.F_VARIANCE(self.x_test[view], threshold = thresholds[view])
                 data_train_variance, mask_train_variance = view_train_variance.apply_variance()
+                data_val_variance, mask_val_variance = view_val_variance.apply_variance()
                 data_test_variance, mask_test_variance = view_test_variance.apply_variance()
-             #   variance_selected_features.append([DataInputNew.features[view][index] for index in mask_variance]) #TODO : wenn man feature namen kennen will
                 variance_train_tensors.append(torch.tensor(data_train_variance))
+                variance_val_tensors.append(torch.tensor(data_val_variance))
                 variance_test_tensors.append(torch.tensor(data_test_variance))
+
+            # We need to set the amount of features for each view across train/val/test sets to the same value
+            # for the neural nets
+            minimum = 10000000
+            for c in range(len(self.view_names)):
+
+                temp = min([variance_train_tensors[c].size(1), variance_val_tensors[c].size(1), variance_test_tensors[c].size(1)])
+                if temp < minimum:
+                    minimum = temp
+
+            for c in range(len(self.view_names)):
+                variance_train_tensors[c] = variance_train_tensors[c][:,0:minimum]
+                variance_val_tensors[c] = variance_val_tensors[c][:,0:minimum]
+                variance_test_tensors[c] = variance_test_tensors[c][:,0:minimum]
+
 
 
 
@@ -419,6 +470,11 @@ class SurvMultiOmicsDataModule(pl.LightningDataModule):
                                               self.duration_test,
                                               self.event_test,
                                               type = 'processed')
+
+            self.val_set = MultiOmicsDataset(variance_val_tensors,
+                                             self.duration_val,
+                                             self.event_val,
+                                             type = 'processed')
 
 
 

GCN.py
@@ -20,6 +20,8 @@ from torch_geometric.nn import GCNConv, SAGEConv, GraphConv, SAGPooling
 from torch_geometric.nn import global_max_pool as gmp
 import math
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+
 
 
 
@@ -129,7 +131,8 @@ def normalize_by_column(data):
 
 
 
-def train(module,views, batch_size =128, n_epochs = 512, lr_scheduler_type = 'onecyclecos', l2_regularization = False, feature_names = None, batch_size_validation=20):
+def train(module,views, batch_size =128, n_epochs = 512, lr_scheduler_type = 'onecyclecos', l2_regularization = False,
+          feature_names = None, batch_size_validation=20, cross_validation_bool = False):
     """
 
     :param module: basically the dataset to be used
@@ -137,6 +140,8 @@ def train(module,views, batch_size =128, n_epochs = 512, lr_scheduler_type = 'on
     :param n_epochs: number of epochs
     :param lr_scheduler_type: type of lr_scheduler : 'lambda','mulitplicative','step','multistep','exponential',...
     :param l2_regularization : bool whether should be applied or not
+    :param cross_validation_bool : bool whether cross validation is to be applied ; False means that we just have cross
+                                   validation with one split (one validation set)
     """
 
 
@@ -192,73 +197,240 @@ def train(module,views, batch_size =128, n_epochs = 512, lr_scheduler_type = 'on
     num_nodes = len(proteins_used)
 
 
-    # normalizing data by rows (genes)
-
-    # reshape not necessary as data already implemented in this manner
-    # features_used = features_used.reshape(-1, num_nodes, num_features)
-    # first dimensions (samples) is derived, so that 2/3 dimension we have [...], [...]
-    # where each list has num_features elements and we have num_nodes in total
-
-    for i in range(num_features):
-        train_data[:,:,i] = normalize(train_data[:,:,i])
-        val_data[:,:,i] = normalize(val_data[:,:,i])
-        test_data[:,:,i] = normalize(test_data[:,:,i])
-
-
-    # reshape structure for use of GCN
-    train_data = train_data.reshape(-1, num_nodes * num_features)
-    val_data = val_data.reshape(-1, num_nodes * num_features)
-    test_data = test_data.reshape(-1, num_nodes * num_features)
-
-    callbacks = [tt.callbacks.EarlyStopping(patience=10)]
-
-    val_full = (val_data, (val_duration,val_event))
-    train_de_pycox = (train_duration, train_event)
-
-
-    torch.manual_seed(0)
     edge_index = torch.LongTensor(edge_index).to(device)
-    net = GCN(len(proteins_used), edge_index, in_features=num_features,n_hidden_layer_dims=[1024,512,256],
-              activ_funcs=['sigmoid','sigmoid','sigmoid']).to(device)
-
-    model = CoxPH(net, tt.optim.Adam(0.001))
-
-    log = model.fit(train_data,train_de_pycox, batch_size, n_epochs, callbacks, verbose=True,
-                    val_data=val_full, val_batch_size= batch_size_validation)
-
-    train = train_data, train_de_pycox
-
-    _ = model.compute_baseline_hazards(*train)
-
-    surv = model.predict_surv_df(test_data)
 
 
-    # Plot it
-    surv.iloc[:, :5].plot()
-    plt.ylabel('S(t | x)')
-    _ = plt.xlabel('Time')
 
-    # test_dur and test_ev need to be numpy arrays for EvalSurv()
-    test_duration = test_duration.numpy()
-    test_event = test_event.numpy()
+    if cross_validation_bool == True:
+        # Cross-Validation based on censored & uncensored data
+        censored_index = []
+        uncensored_index = []
+        # We first need to concatenate all the data (train,val,test) together again
+        full_data = torch.cat(tuple([train_data,val_data,test_data]),dim=0)
+        # We do the same for the duration and event values
+        full_duration = torch.cat(tuple([train_duration,val_duration,test_duration]),dim=0)
+        full_event = torch.cat(tuple([train_event,val_event,test_event]),dim=0)
 
-    ev = EvalSurv(surv, test_duration, test_event, censor_surv='km')
+        # k is the number of folds
+        k = 5
+
+        splits = KFold(n_splits=k,shuffle=True,random_state=42)
 
 
-    # concordance
-    concordance_index = ev.concordance_td()
+        for fold, (train_idx,val_idx) in enumerate(splits.split(np.arange(len(full_data)))):
 
-    #brier score
-    time_grid = np.linspace(test_duration.min(), test_duration.max(), 100)
-    _ = ev.brier_score(time_grid).plot
-    brier_score = ev.integrated_brier_score(time_grid)
+            print('Fold {}'.format(fold + 1))
 
-    #binomial log-likelihood
-    binomial_score = ev.integrated_nbll(time_grid)
+            train_sampler = SubsetRandomSampler(train_idx) # np.array e.g. [0 2 4 8 18 22 ..]
+            test_sampler = SubsetRandomSampler(val_idx)
 
-    print("Concordance index : {} , Integrated Brier Score : {} , Binomial Log-Likelihood : {}".format(concordance_index,
-                                                                                                       brier_score,
-                                                                                                       binomial_score))
+
+            # we take a subset of our train samples for the validation set
+            # 20% of train is validation set
+            # We first need to turn the train_sampler into a np.array (else the method won't work)
+            train_sampler_array = np.empty(len(train_sampler))
+
+            for c,idx in enumerate(train_sampler):
+                np.put(train_sampler_array,c,idx)
+
+            val_sampler = np.random.choice(train_sampler_array, int(0.2 * len(train_sampler)))
+
+            #change dtype
+            val_sampler = val_sampler.astype('int32')
+            train_sampler_array = train_sampler_array.astype('int32')
+
+            # get indices as list
+            val_sampler_list = val_sampler.tolist()
+            # remove these from train sampler
+            train_sampler_array = np.setdiff1d(train_sampler_array, val_sampler_list)
+
+
+            # get training data
+
+            train_data = []
+            train_duration = []
+            train_event = []
+
+            for idx in train_sampler_array:
+                train_data.append(full_data[idx])
+                train_duration.append(full_duration[idx])
+                train_event.append(full_event[idx])
+
+            train_data = torch.stack(tuple(train_data))
+            train_duration = torch.stack(tuple(train_duration))
+            train_event = torch.stack(tuple(train_event))
+
+            # get validation data
+            val_data = []
+            val_duration = []
+            val_event = []
+            for idx in val_sampler:
+                val_data.append(full_data[idx])
+                val_duration.append(full_duration[idx])
+                val_event.append(full_event[idx])
+
+            val_data = torch.stack(tuple(val_data))
+            val_duration = torch.stack(tuple(val_duration))
+            val_event = torch.stack(tuple(val_event))
+
+
+            # get testing data
+            test_data = []
+            test_duration = []
+            test_event = []
+            for idx in test_sampler:
+                test_data.append(full_data[idx])
+                test_duration.append(full_duration[idx])
+                test_event.append(full_event[idx])
+
+            test_data = torch.stack(tuple(test_data))
+            test_duration = torch.stack(tuple(test_duration))
+            test_event = torch.stack(tuple(test_event))
+
+
+            # Normalize data by rows (genes)
+
+            for i in range(num_features):
+                train_data[:,:,i] = normalize(train_data[:,:,i])
+                val_data[:,:,i] = normalize(val_data[:,:,i])
+                test_data[:,:,i] = normalize(test_data[:,:,i])
+
+
+            # reshape structure for use of GCN
+            train_data = train_data.reshape(-1, num_nodes * num_features)
+            val_data = val_data.reshape(-1, num_nodes * num_features)
+            test_data = test_data.reshape(-1, num_nodes * num_features)
+
+
+            callbacks = [tt.callbacks.EarlyStopping(patience=5)]
+
+            val_full = (val_data, (val_duration,val_event))
+            train_de_pycox = (train_duration, train_event)
+
+
+            torch.manual_seed(0)
+
+            net = GCN(len(proteins_used), edge_index, in_features=num_features,n_hidden_layer_dims=[1024,512,256],
+                      activ_funcs=['relu','relu','relu']).to(device)
+
+            model = CoxPH(net, tt.optim.Adam(0.001))
+
+            log = model.fit(train_data,train_de_pycox, batch_size, n_epochs, callbacks, verbose=True,
+                            val_data=val_full, val_batch_size= batch_size_validation)
+
+            train = train_data, train_de_pycox
+
+            _ = model.compute_baseline_hazards(*train)
+
+            surv = model.predict_surv_df(test_data)
+
+
+            # Plot it
+            surv.iloc[:, :5].plot()
+            plt.ylabel('S(t | x)')
+            _ = plt.xlabel('Time')
+
+            # test_dur and test_ev need to be numpy arrays for EvalSurv()
+            test_duration = test_duration.numpy()
+            test_event = test_event.numpy()
+
+            ev = EvalSurv(surv, test_duration, test_event, censor_surv='km')
+
+
+            # concordance
+            concordance_index = ev.concordance_td()
+
+            #brier score
+            time_grid = np.linspace(test_duration.min(), test_duration.max(), 100)
+            _ = ev.brier_score(time_grid).plot
+            brier_score = ev.integrated_brier_score(time_grid)
+
+            #binomial log-likelihood
+            binomial_score = ev.integrated_nbll(time_grid)
+
+            print("Concordance index : {} , Integrated Brier Score : {} , Binomial Log-Likelihood : {}".format(concordance_index,
+                                                                                                               brier_score,
+                                                                                                               binomial_score))
+
+
+
+
+
+
+
+
+
+
+
+
+    else:
+        # normalizing data by rows (genes)
+
+        # reshape not necessary as data already implemented in this manner
+        # features_used = features_used.reshape(-1, num_nodes, num_features)
+        # first dimensions (samples) is derived, so that 2/3 dimension we have [...], [...]
+        # where each list has num_features elements and we have num_nodes in total
+
+        for i in range(num_features):
+            train_data[:,:,i] = normalize(train_data[:,:,i])
+            val_data[:,:,i] = normalize(val_data[:,:,i])
+            test_data[:,:,i] = normalize(test_data[:,:,i])
+
+
+        # reshape structure for use of GCN
+        train_data = train_data.reshape(-1, num_nodes * num_features)
+        val_data = val_data.reshape(-1, num_nodes * num_features)
+        test_data = test_data.reshape(-1, num_nodes * num_features)
+
+        callbacks = [tt.callbacks.EarlyStopping(patience=2)]
+
+        val_full = (val_data, (val_duration,val_event))
+        train_de_pycox = (train_duration, train_event)
+
+
+        torch.manual_seed(0)
+        edge_index = torch.LongTensor(edge_index).to(device)
+        net = GCN(len(proteins_used), edge_index, in_features=num_features,n_hidden_layer_dims=[128,64],
+                  activ_funcs=['sigmoid','sigmoid']).to(device)
+
+        model = CoxPH(net, tt.optim.Adam(0.0001))
+
+        log = model.fit(train_data,train_de_pycox, batch_size, n_epochs, callbacks, verbose=True,
+                        val_data=val_full, val_batch_size= batch_size_validation)
+
+        train = train_data, train_de_pycox
+
+        _ = model.compute_baseline_hazards(*train)
+
+        surv = model.predict_surv_df(test_data)
+
+
+        # Plot it
+        surv.iloc[:, :5].plot()
+        plt.ylabel('S(t | x)')
+        _ = plt.xlabel('Time')
+
+        # test_dur and test_ev need to be numpy arrays for EvalSurv()
+        test_duration = test_duration.numpy()
+        test_event = test_event.numpy()
+
+        ev = EvalSurv(surv, test_duration, test_event, censor_surv='km')
+
+
+        # concordance
+        concordance_index = ev.concordance_td()
+
+        #brier score
+        time_grid = np.linspace(test_duration.min(), test_duration.max(), 100)
+        _ = ev.brier_score(time_grid).plot
+        brier_score = ev.integrated_brier_score(time_grid)
+
+        #binomial log-likelihood
+        binomial_score = ev.integrated_nbll(time_grid)
+
+        print("Concordance index : {} , Integrated Brier Score : {} , Binomial Log-Likelihood : {}".format(concordance_index,
+                                                                                                           brier_score,
+                                                                                                           binomial_score))
 
 
 
@@ -270,8 +442,8 @@ if __name__ == '__main__':
     views = cancer_data[0][2]
     feature_names = cancer_data[0][3]
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    train(module= multimodule,views= views, l2_regularization=True, feature_names=feature_names,batch_size=16,n_epochs=100,
-          batch_size_validation=5)
+    train(module= multimodule,views= views, l2_regularization=True, feature_names=feature_names,batch_size=64,n_epochs=100,
+          batch_size_validation=16,cross_validation_bool=True)
 
 
 

NN.py
@@ -271,7 +271,7 @@ class NN_changeable(nn.Module):
 
 
 
-def train(module,views,device, batch_size =128, n_epochs = 512, lr_scheduler_type = 'onecyclecos'):
+def train(module,device, batch_size =128, n_epochs = 512, lr_scheduler_type = 'onecyclecos'): # TODO :CHANGE ÜBERALL
     """
 
     :param module: basically the dataset to be used
@@ -284,12 +284,12 @@ def train(module,views,device, batch_size =128, n_epochs = 512, lr_scheduler_typ
 
 
     # Setup all the data
-    n_train_samples, n_test_samples, n_val_samples = module.setup()
+    n_train_samples, n_test_samples, n_val_samples, view_names = module.setup() # TODO : CHANGE ÜBERALL
 
 
 
     #Select method for feature selection
-    module.feature_selection(method='pca')
+    module.feature_selection(method='variance')
 
     # Load Dataloaders
     trainloader = module.train_dataloader(batch_size=n_train_samples) # all training examples
@@ -408,11 +408,11 @@ def train(module,views,device, batch_size =128, n_epochs = 512, lr_scheduler_typ
 
 
     # Call NN
-    net = NN_changeable(views,dimensions,feature_offsets,[[10,5],[10,5],[10,5],[10,5]],
-                        [['relu'],['relu'],['relu'],['relu'],['none']], 0.1,
+    net = NN_changeable(view_names,dimensions,feature_offsets,[[10,5] for i in range(len(view_names))],
+                        [['relu'],['relu'],['relu'],['none']], 0.1,
                         [['yes','yes'],['yes','yes',],['yes', 'yes'],['yes','yes']],
                         [['yes','yes'],['yes','yes'],['yes','yes'],['yes','yes']],
-                        dropout_bool=True,batch_norm_bool=False)
+                        dropout_bool=False,batch_norm_bool=False)
     #TODO : Batch norm problem (see AE)
 
 #    net = NN_changeable(views, dimensions, feature_offsets_train,
@@ -512,7 +512,7 @@ def train(module,views,device, batch_size =128, n_epochs = 512, lr_scheduler_typ
 if __name__ == '__main__':
     cancer_data = ReadInData.readcancerdata()
     multimodule = DataInputNew.SurvMultiOmicsDataModule(cancer_data[0][0],cancer_data[0][1],cancer_data[0][2],onezeronorm_bool=False)
-    views = cancer_data[0][2]
+   # views = cancer_data[0][2] # TODO: CHANGE ÜBERALL
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    train(module= multimodule,views= views, device=device, batch_size=32, n_epochs=100)
+    train(module= multimodule, device=device, batch_size=32, n_epochs=100) # TODO : CHANGE ÜBERALL (views raus)
 